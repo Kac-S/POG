c3cabf5902ca5eed50d8ad0153f32724ea3051ce
KSANDSIK <>
2021-09-08 09:09:07 +0000

    Initial commit

---
+++ lib.py
@@ -0,0 +1,29 @@
+def INT(x):
+  for i in range(len(x)):
+    x[i] = int(x[i])
+  return x
+
+def ARRAY(l):
+  a = [0]
+  for i in range(1, l):
+    a.append(0)
+  return a
+
+def POWER(a, n):
+  if n == 0:
+    return 1
+  if n % 2 == 1:
+    return a * (POWER(a, (n - 1)/2))**2
+  else:
+    (POWER(a, n/2))**2
+  return (POWER(a, n/2))**2
+  
+def FACT(n):
+  c = []
+  k = 2
+  while n != 1:
+    while n % k == 0:
+      n //= k
+      c.append(k)
+    k += 1
+  return c
---
+++ main.py
@@ -0,0 +1,5 @@
+x = int(input())
+if x < 331777:
+  print(331776)
+else:
+  print(-1)
