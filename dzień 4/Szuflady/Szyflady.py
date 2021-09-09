86138b54aeda7def262fd331daf6a7052bc77ea1
Kacper <Kacper.sikora@life.pl>
2021-09-09 11:30:01 +0200

    .

---
+++ dzień 4/Szuflady/Szyflady.py
@@ -0,0 +1,24 @@
+
+def INT(x):
+  for i in range(len(x)):
+    x[i] = int(x[i])
+  return x
+
+n = int(input())
+x = list(reversed(INT(input().split())))
+a = x[0] + 1
+c = 0
+
+
+for i in x:
+    if i < a:
+        a = i
+    else:
+        a -= 1
+        c += 1
+
+if a < 0:
+    print(-1)
+else:
+    print(c)
+
