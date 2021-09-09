4e1367687b51773b31e065d784b0daa1ce29ace4
Kacper <Kacper.sikora@life.pl>
2021-09-09 10:37:28 +0200

    .

--- dzień 4/Podzbiory/podzbiory.py
+++ dzień 4/Podzbiory/podzbiory.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import itertools
 n = int(input())
 lst = [list(i) for i in itertools.product([0, 1], repeat=n)]
@@ -7,6 +6,3 @@ for x in lst:
     for i in x:
         pr+=str(i)
     print(pr)
-	
-=======
->>>>>>> 67964fea3bf0b72eb9c1aa08db8a6649d7c720c3
