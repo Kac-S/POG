261c75211eec7b746854295fddf9313c4ecdd657
Kacper <Kacper.sikora@life.pl>
2021-09-09 11:48:51 +0200

    .

--- lib.py
+++ lib.py
@@ -3,14 +3,6 @@ def INT(x):
     x[i] = int(x[i])
   return x
 
-def ARRAY(l):
-  a = [0]
-  for i in range(1, l):
-    a.append(0)
-  return a
-
- # a = []*l
-
 def POWER(a, n):
   if n == 0:
     return 1
