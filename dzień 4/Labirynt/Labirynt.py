27497a3ff99f980d4f65888c32ad2725f4f1f8d5
Artur Panek <47123591+artur-panek@users.noreply.github.com>
2021-09-09 10:33:40 +0000

    labiryn NOWY REKORD KURWA

--- dzień 4/Labirynt/Labirynt.py
+++ dzień 4/Labirynt/Labirynt.py
@@ -1,35 +1,30 @@
-#przedstawiam niesamowity algorytm, zdolny do zdobycia 1 punktu na 100 możliwych
+#przedstawiam nową wersję niesamowitego algorytmu, zdolną do zdobycia 3 punktów na 100 możliwych
 # jeżeli go naprawisz, to jesteś bogolem, bo ja już nie potrafię
 # P.S mamusiu ratuj
 #n, m = tuple(map(int, input().split()))
 #k = []
 #for x in range(n):
 #	k += list(map(int, input().split()))
-n, m = 3, 4
-k = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-print(k)
-odw = [k[-1]]
-nast = k[-1]
-energia = 0
+n, m = tuple(map(int, input().split()))
+k = []
+for x in range(n):
+	k += list(map(int, input().split()))
 
-if nast == 1:
-	print(1)
-	exit()
+kom = k[-1]
+odw = []
+energia = 1
 while True:
-	kom = k[nast-1]
-	odw.append(kom)
-	print("O", odw)
 	if kom == 1:
 		print(energia)
 		break
-		exit()
 	elif kom in odw:
-		print('NIE')
+		print("NIE")
 		break
-		exit()
 	else:
-		nast = kom
-	energia += 1
+		energia += 1
+		odw.append(kom)
+		kom = k[kom-1]
+exit()
 
 
 
